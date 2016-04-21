f0951ff434fa83e20150a1c67d2fbe161a823074
LisaMabley <lisamabley@gmail.com>
2016-04-21 13:51:08 -0500

    Add put route in people router

--- client/client.js
+++ client/client.js
@@ -9,21 +9,27 @@ app.controller('MainController', ['$http', function($http) {
   controller.getPats = function() {
     $http.get('/patronus').then(function(response) {
       controller.patList = response.data;
+      controller.patName = '';
     });
   }
 
   controller.getPeople = function() {
-    $http.get('/people').then(function(response) {
+    $http.get('/people')
+    .then(function(response) {
       controller.peopleList = response.data;
+      controller.personLast = '';
+      controller.personFirst = '';
     });
   }
 
   controller.addPatronus = function(){
-    $http.post('/patronus', {name: controller.patName}).then(controller.getPats());
+    $http.post('/patronus', {name: controller.patName})
+    .then(controller.getPats);
   }
 
   controller.addPerson = function(){
-    $http.post('/people', {first_name: controller.personFirst, last_name: controller.personLast}).then(controller.getPeople());
+    $http.post('/people', {first_name: controller.personFirst, last_name: controller.personLast})
+    .then(controller.getPeople);
   }
 
   controller.getPats();
--- server/routes/people.js
+++ server/routes/people.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
     }else{
       var query = client.query('SELECT first_name, last_name FROM people');
       var results = [];
+      console.log('hi from the people router');
 
       query.on('row', function(row){
         results.push(row);
@@ -31,6 +32,50 @@ router.get('/', function(req, res) {
   })
 });
 
-// add Post route here
+router.post('/', function(request, response){
+  pg.connect(connectionString, function(err, client, done){
+    if(err){
+      console.log(err);
+      response.sendStatus(500);
+    } else {
+
+      var first_name = request.body.first_name;
+      var last_name = request.body.last_name;
+      var query = client.query('INSERT INTO people (first_name, last_name) VALUES ($1, $2)', [first_name, last_name]);
+
+      query.on('end', function(){
+        done();
+        response.sendStatus(200);
+      });
+
+      query.on('error', function(error){
+        console.error('Error running query', error);
+        done();
+        response.sendStatus(500);
+
+      });
+    }
+  });
+});
+
+router.put('/:id', function(request, response) {
+  var id = request.params.id;
+  var patronus = request.body.patronus;
+  pg.connect(connectionString, function(err, client, done) {
+    var query = client.query('UPDATE people SET patronus_id = ' + patronus + 'WHERE id = ' + id + ';');
+
+    query.on('end', function() {
+      done();
+      response.sendStatus(200);
+    });
+
+    query.on('error', function(err) {
+      console.error(err);
+      done();
+      response.sendStatus(500);
+    });
+  });
+});
+
 
 module.exports = router;
